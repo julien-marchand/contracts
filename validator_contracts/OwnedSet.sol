c1e361d134fd688dabaffaf243fb7bb3f658ade1
Cole Lightfighter <cole@parity.io>
2018-01-22 19:53:01 +0000

    OwnedSet: fix validator iteration
    Fix off-by-one-error for validator iteration during initialization.

--- validator_contracts/OwnedSet.sol
+++ validator_contracts/OwnedSet.sol
@@ -71,7 +71,7 @@ contract OwnedSet is Owned, ValidatorSet {
 
 	function OwnedSet(address[] _initial) public {
 		pending = _initial;
-		for (uint i = 0; i < _initial.length - 1; i++) {
+		for (uint i = 0; i < _initial.length; i++) {
 			pendingStatus[_initial[i]].isIn = true;
 			pendingStatus[_initial[i]].index = i;
 		}
